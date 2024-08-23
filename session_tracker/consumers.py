0e148ef37a7e980323d2427b7bda2e01327dc1b1
Jim Galvan <58128571+JimGalvan@users.noreply.github.com>
2024-08-23 00:57:03 -0700

    added live sessions

--- session_tracker/consumers.py
+++ session_tracker/consumers.py
@@ -1,16 +1,28 @@
 import json
+import logging
 
 from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 from .models import UserSession
 
+logger = logging.getLogger(__name__)
+
 
 class SessionConsumer(AsyncWebsocketConsumer):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.group_name = None
+        self.session_id = None
+        self.live_session_group_postfix = "live_session"
 
     async def connect(self):
         self.session_id = self.scope['url_route']['kwargs']['session_id']
         self.group_name = f"session_{self.session_id}"
+
+        # Logging connection attempt
+        logger.info(f"SessionConsumer: Connecting to session {self.session_id}")
 
         # Join the session group
         await self.channel_layer.group_add(
@@ -27,6 +39,9 @@ class SessionConsumer(AsyncWebsocketConsumer):
         await self.notify_live_status(True)
 
     async def disconnect(self, close_code):
+        # Logging disconnection
+        logger.info(f"SessionConsumer: Disconnecting from session {self.session_id} with close code {close_code}")
+
         # Leave the session group
         await self.channel_layer.group_discard(
             self.group_name,
@@ -40,11 +55,12 @@ class SessionConsumer(AsyncWebsocketConsumer):
         await self.notify_live_status(False)
 
     async def receive(self, text_data=None, bytes_data=None):
+        logger.info(f"SessionConsumer: Received data in session {self.session_id}")
+
         text_data_json = json.loads(text_data)
         events = text_data_json['events']
         user_id = text_data_json['user_id']
 
-        # Update or create the session
         session, created = await sync_to_async(UserSession.objects.get_or_create)(
             session_id=self.session_id,
             defaults={'user_id': user_id, 'events': events}
@@ -53,14 +69,24 @@ class SessionConsumer(AsyncWebsocketConsumer):
             session.events.extend(events)
             await sync_to_async(session.save)()
 
+        logger.info(f"SessionConsumer: Sending live session event for session {self.session_id}")
+
+        await self.channel_layer.group_send(
+            f"{self.group_name}_{self.live_session_group_postfix}",
+            {
+                "type": "live_session_event",
+                "text": json.dumps(events[-1])  # Send the last event
+            }
+        )
+
         await self.send(text_data=json.dumps({'status': 'success'}))
 
     async def set_session_live_status(self, is_live):
-        """Set the live status of the session."""
+        logger.info(f"SessionConsumer: Setting live status to {is_live} for session {self.session_id}")
         await sync_to_async(UserSession.objects.filter(session_id=self.session_id).update)(live=is_live)
 
     async def notify_live_status(self, is_live):
-        """Notify about the session's live status via another WebSocket."""
+        logger.info(f"SessionConsumer: Notifying live status change to {is_live} for session {self.session_id}")
         await self.channel_layer.group_send(
             "live_status",
             {
@@ -73,7 +99,8 @@ class SessionConsumer(AsyncWebsocketConsumer):
 
 class LiveStatusConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        # Join the live_status group
+        logger.info("LiveStatusConsumer: Connecting to live status updates")
+
         await self.channel_layer.group_add(
             "live_status",  # Group name must match the one used in SessionConsumer
             self.channel_name
@@ -82,19 +109,55 @@ class LiveStatusConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Leave the live_status group
+        logger.info(f"LiveStatusConsumer: Disconnecting from live status updates with close code {close_code}")
+
         await self.channel_layer.group_discard(
             "live_status",
             self.channel_name
         )
 
     async def live_status_update(self, event):
-        # Handle the message sent from SessionConsumer
+        logger.info(
+            f"LiveStatusConsumer: Received live status update for session {event['session_id']} with status {event['live']}")
+
         session_id = event['session_id']
         live = event['live']
 
-        # Send the live status update to the WebSocket client
         await self.send(text_data=json.dumps({
             'session_id': session_id,
             'live': live
         }))
+
+
+class LiveSessionConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.session_id = self.scope['url_route']['kwargs']['session_id']
+        self.group_name = f"session_{self.session_id}"
+        self.group_postfix: str = "live_session"
+
+        logger.info(f"LiveSessionConsumer: Connecting to live session {self.session_id}")
+
+        # Join the session group
+        await self.channel_layer.group_add(
+            f"{self.group_name}_{self.group_postfix}",
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        logger.info(
+            f"LiveSessionConsumer: Disconnecting from live session {self.session_id} with close code {close_code}")
+
+        # Leave the session group
+        await self.channel_layer.group_discard(
+            self.group_name + self.group_postfix,
+            self.channel_name
+        )
+
+    async def live_session_event(self, event):
+        """Handle the live session event."""
+        logger.info(f"LiveSessionConsumer: Received live session event for session {self.session_id}")
+        logger.debug(f"Event data: {event}")
+
+        await self.send(text_data=event['text'])
--- session_tracker/routing.py
+++ session_tracker/routing.py
@@ -1,7 +1,9 @@
 from django.urls import re_path
-from .consumers import SessionConsumer, LiveStatusConsumer
+
+from .consumers import SessionConsumer, LiveStatusConsumer, LiveSessionConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/record-session/(?P<session_id>\w+)/$', SessionConsumer.as_asgi()),
+    re_path(r'ws/live-session/(?P<session_id>\w+)/$', LiveSessionConsumer.as_asgi()),
     re_path(r'ws/live-status/$', LiveStatusConsumer.as_asgi()),
 ]
