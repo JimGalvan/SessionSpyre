6eada1570f5cb82b124244eb509c734e50585d27
Jim Galvan <58128571+JimGalvan@users.noreply.github.com>
2024-09-06 20:19:17 -0700

    add date picker

--- session_tracker/views/session_views.py
+++ session_tracker/views/session_views.py
@@ -1,4 +1,5 @@
-from datetime import timedelta, datetime
+from datetime import datetime
+from datetime import timedelta
 
 import pytz
 from django.shortcuts import render
@@ -10,7 +11,8 @@ def sessions_view(request, site_id):
     user_id: str = request.user.id
     site: Site = Site.objects.get(id=site_id)
     sessions: list = UserSession.objects.filter(user_id=user_id, site=site)
-    return render(request, 'sessions/sessions.html', {'site': site, 'sessions': sessions})
+    today_date = datetime.now().date()
+    return render(request, 'sessions/sessions.html', {'site': site, 'sessions': sessions, 'today_date': today_date})
 
 
 def replay_session(request, session_id):
@@ -27,10 +29,18 @@ def delete_session(request, session_id):
 
 
 def sessions_list(request, site_id):
-    user_id: str = request.user.id
-    sessions: list = UserSession.objects.filter(user_id=user_id, site_id=site_id)
+    user_id = request.user.id
+    date_str = request.GET.get('date')
+
+    if date_str:
+        selected_date = datetime.strptime(date_str, '%Y-%m-%d').date()
+        sessions = UserSession.objects.filter(user_id=user_id, site_id=site_id, created_at__date=selected_date)
+    else:
+        sessions = UserSession.objects.filter(user_id=user_id, site_id=site_id)
+
     check_live_status(sessions)
-    return render(request, 'sessions/session_list.html', {'sessions': sessions})
+    current_date = datetime.now().date()
+    return render(request, 'sessions/session_list.html', {'sessions': sessions, 'current_date': current_date})
 
 
 def check_live_status(sessions):
