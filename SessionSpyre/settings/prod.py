0ea2f9edd58e07fe9d8f5c62f013a0c865c0afe0
Jim Galvan <58128571+JimGalvan@users.noreply.github.com>
2024-09-15 18:16:37 -0700

    update settings

--- SessionSpyre/settings/prod.py
+++ SessionSpyre/settings/prod.py
@@ -1,30 +1,24 @@
 from .base import *
-import environ
 import os
-
-# Initialize environ
-env = environ.Env(
-    DEBUG=(bool, False)
-)
 
 DEBUG = True
 
-# Reading .env file
-# environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+# Reading .os.getenv file
+# environ.Env.read_env(os.path.join(BASE_DIR, '.os.getenv'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(env('SECRET_KEY'))
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': env('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST', default='localhost'),
-        'PORT': env('DB_PORT', default='5432'),
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', default='localhost'),
+        'PORT': os.getenv('DB_PORT', default='5432'),
     }
 }
 
