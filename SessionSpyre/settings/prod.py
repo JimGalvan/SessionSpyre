303877d105557e5a4c2df81e63b1e0b953a32120
Jim Galvan <58128571+JimGalvan@users.noreply.github.com>
2024-09-15 18:53:42 -0700

    update settings

--- SessionSpyre/settings/prod.py
+++ SessionSpyre/settings/prod.py
@@ -1,5 +1,5 @@
-from .base import *
 import os
+import dj_database_url
 
 DEBUG = True
 
@@ -11,6 +11,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 ALLOWED_HOSTS = ['*']
 
+print("DATABASE_URL: ", os.getenv('DATABASE_URL'))
 print("ENGINE: ", os.getenv('DB_ENGINE', default='django.db.backends.postgresql'))
 print("NAME: ", os.getenv('DB_NAME'))
 print("USER: ", os.getenv('DB_USER'))
@@ -18,15 +19,19 @@ print("PASSWORD: ", os.getenv('DB_PASSWORD'))
 print("HOST: ", os.getenv('DB_HOST', default='localhost'))
 print("PORT: ", os.getenv('DB_PORT', default='5432'))
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST', default='localhost'),
+#         'PORT': os.getenv('DB_PORT', default='5432'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', default='localhost'),
-        'PORT': os.getenv('DB_PORT', default='5432'),
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 CSRF_TRUSTED_ORIGINS = [
