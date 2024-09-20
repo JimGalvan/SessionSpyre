ea9b7d8340947d089744623853f9afe12c91363c
Jim Galvan <58128571+JimGalvan@users.noreply.github.com>
2024-09-20 16:40:54 -0700

    add redis

--- SessionSpyre/settings/prod.py
+++ SessionSpyre/settings/prod.py
@@ -1,11 +1,17 @@
-from .base import *
 import os
+
 import dj_database_url
 
-DEBUG = True
+DEBUG = False
 
-# Reading .os.getenv file
-# environ.Env.read_env(os.path.join(BASE_DIR, '.os.getenv'))
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [os.getenv('REDIS_URL')],  # Use the Redis URL from Railway
+        },
+    },
+}
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
@@ -19,17 +25,6 @@ print("USER: ", os.getenv('DB_USER'))
 print("PASSWORD: ", os.getenv('DB_PASSWORD'))
 print("HOST: ", os.getenv('DB_HOST', default='localhost'))
 print("PORT: ", os.getenv('DB_PORT', default='5432'))
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST', default='localhost'),
-#         'PORT': os.getenv('DB_PORT', default='5432'),
-#     }
-# }
 
 DATABASES = {
     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
