e6b0b98077aa240c20da83d0f79cf374e3d4563f
Jim Galvan <58128571+JimGalvan@users.noreply.github.com>
2026-03-24 20:49:17 -0700

    small package import

--- SessionSpyre/asgi.py
+++ SessionSpyre/asgi.py
@@ -7,7 +7,7 @@ from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 # Use DJANGO_ENV environment variable to determine which settings to use
-django_env = os.environ.get('DJANGO_ENV', 'production')
+django_env = os.environ.get('DJANGO_ENV', 'prod')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'SessionSpyre.settings.{django_env}')
 
 django.setup()
